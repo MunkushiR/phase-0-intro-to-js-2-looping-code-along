97d965dec6137071b49b946ae8ea7c89f0ae019d
MunkushiR <ruth.munkushi@student.moringaschool.com>
2023-03-11 10:28:45 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solutions in this file
+var numberList = [];
+for (var i = 0; i <= 5; i++) {
+  numberList.push(i);
+}
+console.log(numberList);
+numberList.pop();
+console.log(numberList);
+
